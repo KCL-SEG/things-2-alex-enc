ab1d8589b3f480e3774bc7cda92add72fb9d8952
Alexandra Encarnacion <alex1102e@gmail.com>
2022-11-02 16:46:22 +0000

    Create ThingsForm

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,16 @@
 """Forms of the project."""
 
 # Create your forms here.
+# the project includes a nearly empty file called things/forms.py.
+# In this file, define a form called ThingForm, that contains the fields name, description, and quantity, but not created_at.
+#
+# The form must accept valid inputs for Thing and reject invalid input for Thing.
+#
+# The description field must be displayed as a Textarea. The quantity field must be displayed as NumberInput.
+from django import forms
+from .models import User
+
+class ThingForm(forms.Form):
+        name = forms.CharField(label="Name")
+        description = forms.Textarea(label="Description")
+        quantity = forms.NumberInput(label="Quantity")
